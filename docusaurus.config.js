f110d133efc82bf19be3f36376c917db2455c61d
ukdaaan <git@daaan.dev>
2023-02-05 14:42:36 +0000

    upgrade to docu 2.3.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,34 @@ const config = {
 
   plugins: [
     [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+    [
       '@docusaurus/plugin-content-docs',
       {
         id: 'guides',
