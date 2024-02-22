9975bb9cad678a967cf601895a6141d76195a847
Daniel <33575896+ukdaaan@users.noreply.github.com>
2024-02-22 13:26:25 +0000

    astro v4 upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,61 +1,71 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.setup.md',
-  trailingSlash: 'never',
+  site: "https://www.setup.md",
+  trailingSlash: "never",
   build: {
-    format: 'file'
+    format: "file",
   },
-  integrations: [starlight({
-    title: 'setup.md',
-    editLink: {
-      baseUrl: 'https://github.com/setupmd/docs/edit/v3'
-    },
-    social: {
-      discord: 'https://link.setup.md/discord',
-      github: 'https://link.setup.md/repo',
-      patreon: 'https://link.setup.md/patreon'
-    },
-    sidebar: [{
-      label: 'Project Information',
-      autogenerate: {
-        directory: '/information/'
+  integrations: [
+    starlight({
+      title: "setup.md",
+      editLink: {
+        baseUrl: "https://github.com/setupmd/docs/edit/v3",
       },
-      collapsed: false
-    }, {
-      label: 'Enhancements',
-      autogenerate: {
-        directory: '/enhancements/'
+      social: {
+        discord: "https://link.setup.md/discord",
+        github: "https://link.setup.md/repo",
       },
-      collapsed: true
-    }, {
-      label: 'Hosting Tips',
-      autogenerate: {
-        directory: '/hosting/'
-      },
-      collapsed: true
-    }, {
-      label: 'Server Tools',
-      autogenerate: {
-        directory: '/tools/'
-      },
-      collapsed: true
-    }, {
-      label: 'Server Troubleshooting',
-      autogenerate: {
-        directory: '/troubleshooting/'
-      },
-      collapsed: true
-    }, {
-      label: 'Guides',
-      autogenerate: {
-        directory: '/guides/'
-      },
-      collapsed: true
-    }]
-  }), vue(), tailwind()],
+      sidebar: [
+        {
+          label: "Project Information",
+          autogenerate: {
+            directory: "/information/",
+          },
+          collapsed: false,
+        },
+        {
+          label: "Enhancements",
+          autogenerate: {
+            directory: "/enhancements/",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Hosting Tips",
+          autogenerate: {
+            directory: "/hosting/",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Server Tools",
+          autogenerate: {
+            directory: "/tools/",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Server Troubleshooting",
+          autogenerate: {
+            directory: "/troubleshooting/",
+          },
+          collapsed: true,
+        },
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "/guides/",
+          },
+          collapsed: true,
+        },
+      ],
+    }),
+    vue(),
+    tailwind(),
+  ],
 });
